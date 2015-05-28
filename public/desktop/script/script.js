350aecfb55908b897439a07f44815d79d88017bc
AdrianiBruss <abrussolo@gmail.com>
2015-05-28 18:22:25 +0200

    works on smartphone

--- public/desktop/script/script.js
+++ public/desktop/script/script.js
@@ -15,20 +15,19 @@ $(function(){
     var secret_key = randomString();
     var hash = CryptoJS.SHA512(secret_key);
 
-    var socket = io('ws://192.168.20.253:3303');
-    //var socket = io('ws://192.168.10.17:3303');
+    //var socket = io('ws://192.168.20.253:3303');
+    var socket = io('ws://192.168.10.16:3303');
 
     console.log(hash.words[0]);
 
-    socket.emit('setKey', hash.words[0]);
-
+    socket.emit('desktopCo', hash.words[0]);
 
     // impements methods that receive data from server and execute them
     socket.on('testDesk', function(data){
         console.log(data);
     });
 
-    //$('#code').append('<a href="http://192.168.10.17:9000/mobileView/' + secret_key + '">Check for mobile</a>');
-    $('#code').append('<a href="http://192.168.20.253:3300/public/mobile/">Enter the code : '+secret_key+'</a>');
+    //$('#code').append('<a href="http://192.168.20.253:3300/public/mobile/">Enter the code : '+secret_key+'</a>');
+    $('#code').append('<a href="http://192.168.10.16:3300/public/mobile/">Enter the code : '+secret_key+'</a>');
 
 });
--- public/mobile/scripts/script.js
+++ public/mobile/scripts/script.js
@@ -2,8 +2,8 @@ $(function(){
     'use strict';
 
     // connecter au port
-    //var socket = io('ws://192.168.10.17:3303');
-    var socket = io('ws://192.168.20.253:3303');
+    var socket = io('ws://192.168.10.16:3303');
+    //var socket = io('ws://192.168.20.253:3303');
 
     var data = {
         "user"      : 'Adrien',
@@ -15,7 +15,7 @@ $(function(){
 
         console.log('click');
         socket.emit('test', Math.floor((Math.random() * 10) + 1));
-        //$.post("/postest", { message: 'message posted ! ' });
+        //$.post("/postest", { message: Math.floor((Math.random() * 10) + 1) });
 
     });
 
@@ -23,10 +23,22 @@ $(function(){
 
     $('#submit').on('click', function(){
 
-        console.log($('#input').val());
+        // encrypte la clé en sha512
         key = CryptoJS.SHA512($('#input').val());
-
+        // envoie la clé au serveur
         socket.emit('mobileCo', key.words[0]);
+
+    });
+
+    $('#connected').css('color', 'red');
+
+    // en attente de la connexion du mobile
+    socket.on('mobileConnected', function(res){
+
+        // le mobile est connecté au serveur
+        if(res.data == 'ok'){
+            $('#connected').css('color', 'green');
+        }
 
     })
 
