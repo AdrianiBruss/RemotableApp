bd16cabe5667fa50baca1404451d02ea76d63598
AdrianiBruss <abrussolo@gmail.com>
2015-05-28 12:31:13 +0200

    updated ip

--- public/desktop/script/script.js
+++ public/desktop/script/script.js
@@ -3,16 +3,18 @@ $(function(){
 
     var secret_key = "AFK9";
 
-    var socket = io('ws://192.168.10.17:3303');
+    var socket = io('ws://192.168.20.253:3303');
+    //var socket = io('ws://192.168.10.17:3303');
 
     socket.emit('setKey', secret_key);
 
-    socket.on('scroll', function(data){
 
+    // impements methods that receive data from server and execute them
+    socket.on('testDesk', function(data){
         console.log(data);
-
     });
 
-    $('#code').append('<a href="http://192.168.10.17:9000/mobileView/' + secret_key + '">Check for mobile</a>');
+    //$('#code').append('<a href="http://192.168.10.17:9000/mobileView/' + secret_key + '">Check for mobile</a>');
+    $('#code').append('<a href="http://192.168.20.253:9000/mobileView/' + secret_key + '">Check for mobile</a>');
 
 });
