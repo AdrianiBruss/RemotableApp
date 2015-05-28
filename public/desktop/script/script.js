a38ec866de1e41b01a2b76f35c4c370c67393827
AdrianiBruss <abrussolo@gmail.com>
2015-05-28 16:13:37 +0200

    desktop and mobile on the same server

--- public/desktop/script/script.js
+++ public/desktop/script/script.js
@@ -1,12 +1,26 @@
 $(function(){
     'use strict';
 
-    var secret_key = "AFK9";
+    function randomString()
+    {
+        var text = "";
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+        for( var i=0; i < 4; i++ )
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+        return text;
+    }
+
+    var secret_key = randomString();
+    var hash = CryptoJS.SHA512(secret_key);
 
     var socket = io('ws://192.168.20.253:3303');
     //var socket = io('ws://192.168.10.17:3303');
 
-    socket.emit('setKey', secret_key);
+    console.log(hash.words[0]);
+
+    socket.emit('setKey', hash.words[0]);
 
 
     // impements methods that receive data from server and execute them
@@ -15,6 +29,6 @@ $(function(){
     });
 
     //$('#code').append('<a href="http://192.168.10.17:9000/mobileView/' + secret_key + '">Check for mobile</a>');
-    $('#code').append('<a href="http://192.168.20.253:9000/mobileView/' + secret_key + '">Check for mobile</a>');
+    $('#code').append('<a href="http://192.168.20.253:3300/public/mobile/">Enter the code : '+secret_key+'</a>');
 
 });
--- public/mobile/scripts/script.js
+++ public/mobile/scripts/script.js
@@ -2,7 +2,8 @@ $(function(){
     'use strict';
 
     // connecter au port
-    var socket = io('ws://192.168.10.17:3303');
+    //var socket = io('ws://192.168.10.17:3303');
+    var socket = io('ws://192.168.20.253:3303');
 
     var data = {
         "user"      : 'Adrien',
@@ -13,10 +14,21 @@ $(function(){
     $('#test').on('click', function(){
 
         console.log('click');
-        //socket.emit('test', data);
-        $.post("/postest", { message: 'message posted ! ' });
+        socket.emit('test', Math.floor((Math.random() * 10) + 1));
+        //$.post("/postest", { message: 'message posted ! ' });
 
     });
+
+    var key;
+
+    $('#submit').on('click', function(){
+
+        console.log($('#input').val());
+        key = CryptoJS.SHA512($('#input').val());
+
+        socket.emit('mobileCo', key.words[0]);
+
+    })
 
 
 });
